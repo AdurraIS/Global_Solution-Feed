c0d449dfaf4e727e6cdb8ca687602f596611ae3b
AdurraIS <giovanniultramari@gmail.com>
2023-05-27 03:27:29 -0300

    Adicionando Paragrafo, Botão e Imagem na Section 1

--- solutionfeed/src/Componentes/Home/Home.js
+++ solutionfeed/src/Componentes/Home/Home.js
@@ -1,13 +1,24 @@
 import "./Home.css";
-
-export default function Home(){
-    return(
+import imgearth from "../../Imgs/earthimg.png";
+export default function Home() {
+    return (
         <>
             <div class="homecontainer">
-                <div class="section1">
-                    <div class="titulo-home-section1">Combate à fome mundial</div>
+                <div class="container-section1-section2">
+                    <div class="section1">
+                        <div class="section1-container-left">
+                            <div class="titulo-home-section1">Combate à fome mundial</div>
+                            <p>Abordamos a importância desse tema, aplicamos suas inovações e melhoramos nosso futuro.
+                                Implementamos melhorias na distribuição de alimentos, automação da agricultura,
+                                métodos de melhor gestão do espaço (agricultura verticalizada) e aproveitamento de comida.
+                            </p>
+                            <button class="button-home-section1">Mais sobre nossas inovações</button>
+                        </div>
+                        <div class="section1-container-right">
+                            <img class="section1-img" src={imgearth} alt="Imagem"></img>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </>
     );
